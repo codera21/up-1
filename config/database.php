bc6c14d360dadef1e82881c6f324a04752316c97
codera21 <atul.nepal@gmail.com>
2018-12-29 22:21:04 +0545

    added unix_socket

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', 'root'),
-            'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
+            'unix_socket' => '/var/lib/mysql/mysql.sock',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -59,7 +59,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
