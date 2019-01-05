bd371720b9609f0ab83b0d1bfcb29584803c7192
Ashish336b <ashish336b@gmail.com>
2019-01-05 10:13:01 +0545

    revert

--- app/Http/Controllers/Admin/UserCommissionController.php
+++ app/Http/Controllers/Admin/UserCommissionController.php
@@ -194,12 +194,6 @@ class UserCommissionController extends Controller
                     ),
                 )
             );
-        if (env('SITE')!='ENG')
-        {
-            dump($grid);
-        }else{
-            exit;
-        }
         return view('admin.user-commission.index', ['grid' => $grid]);
     }
 
