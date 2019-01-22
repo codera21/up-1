f16fde75f0b98aaf42430a79fc15751268badb9e
Ashish336b <ashish336b@gmail.com>
2019-01-22 16:17:04 +0545

    Ok

--- resources/views/layouts/backend/partials/sidebar-navigation-items.blade.php
+++ resources/views/layouts/backend/partials/sidebar-navigation-items.blade.php
@@ -1,19 +1,28 @@
 @foreach($items as $item)
-  <li @if($item->hasChildren())@endif >
-    @if($item->link) <a @if($item->hasChildren()) class="treeview" data-toggle="dropdown" @endif href="{!! $item->url() !!}">
-      {!! $item->title !!}
-      @if($item->hasChildren()) <span class="fa arrow"></span> @endif
-    </a>
-    @else
-      {!! $item->title !!}
+    <li @if($item->hasChildren())@endif >
+        @if($item->link) <a @if($item->hasChildren()) class="treeview" data-toggle="dropdown"
+                            @endif href="{!! $item->url() !!}">
+            {!! $item->title !!}
+            @if($item->hasChildren()) <span class="fa arrow"></span> @endif
+        </a>
+        @else
+            {!! $item->title !!}
+        @endif
+        @if($item->hasChildren())
+            <ul class="treeview-menu">
+                @include(config('laravel-menu.views.bootstrap-items'), array('items' => $item->children()))
+            </ul>
+        @endif
+    </li>
+    @if($item->divider)
+        <li{!! Lavary\Menu\Builder::attributes($item->divider) !!}></li>
     @endif
-    @if($item->hasChildren())
-      <ul class="treeview-menu">
-        @include(config('laravel-menu.views.bootstrap-items'), array('items' => $item->children()))
-      </ul> 
-    @endif
-  </li>
-  @if($item->divider)
-  	<li{!! Lavary\Menu\Builder::attributes($item->divider) !!}></li>
-  @endif
 @endforeach
+<script>
+    if (window.outerWidth <= 768) {
+        jQuery('li').click(function () {
+            jQuery('li').removeClass('active');
+            jQuery(this).toggleClass('active');
+        });
+    }
+</script>
