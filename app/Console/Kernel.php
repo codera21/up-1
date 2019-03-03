9ca183d3da905aa4627d1018e90994db30395ce0
Ashish <ashish336b@gmail.com>
2019-03-03 21:34:41 +0545

    ee

---
+++ app/Console/Kernel.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Console;
+use Carbon\Carbon;
+use function foo\func;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        
+    }
+    /**
+     * Register the Closure based commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        require base_path('routes/console.php');
+    }
+}
