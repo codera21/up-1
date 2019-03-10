c1eab58205cfadf137bc5857320dcd30943ac4d6
Ashish <ashish336b@gmail.com>
2019-03-10 14:55:59 +0545

    Blockage page message

--- resources/views/not_active.blade.php
+++ resources/views/not_active.blade.php
@@ -13,6 +13,10 @@
         <a style="color : #fff" type="button" class="btn btn-success"
            href="{{route('online-payment.activate')}}"> {{ __('app.click_here') }}</a>
     </div>
+    <div class="alert alert-info" role="alert">
+        <b class="text-danger">Important</b>: {{trans('app.this_is_how')}}
+    <a href="{{trans('app.this_is_link')}}" style="font-weight:bold;color:blue"> {{ trans('app.click_here') }}</a>
+    </div>
     <br>
     <br>
     <br>
