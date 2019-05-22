126fdb4d26d573d80a8e35f363512a81dfaea4f5
Ashish <ashish336b@gmail.com>
2019-05-22 19:47:56 +0545

    Register api

--- app/Http/Controllers/Api/APiAuthController.php
+++ app/Http/Controllers/Api/APiAuthController.php
@@ -33,8 +33,41 @@ class APiAuthController extends Controller
             echo json_encode($result);
         }
     }
-    public function csrf()
+
+    public function AuthRegister()
     {
-        return view("apiLogin");
+        $first_name = isset($_POST["first_name"]) ? $_POST["first_name"] : "ApiTest";
+        $last_name = isset($_POST["last_name"]) ? $_POST["last_name"] : "TestApi";
+        $address = isset($_POST["address1"]) ? $_POST["address1"] : "AddressApi";
+        $country = isset($_POST["country"]) ? $_POST["country"] : "NepalApi";
+        $sex = isset($_POST["sex"]) ? $_POST["sex"] : "Female";
+        $phone = isset($_POST["phone"]) ? $_POST["phone"] : "111222111";
+        $username = isset($_POST["username"]) ? $_POST["username"] : "apiusername";
+        $email = isset($_POST["email"]) ? $_POST["email"] : "api@gmail.com";
+        $password = isset($_POST["password"]) ? $_POST["password"] : "11111111";
+        $password = bcrypt($password);
+
+        $array = array();
+        $array["register"] = array();
+
+        $query = DB::table("users")->insert([
+            "first_name" => $first_name,
+            "last_name" => $last_name,
+            "address1" => $address,
+            "country" => $country,
+            "sex" => $sex,
+            "phone" => $phone,
+            "username" => $username,
+            "email" => $email,
+            "password" => $password,
+            'verified' => 1,
+            'not_now' => 1,
+            'is_active' => 'YES',
+            'parent_id' => '2'
+        ]);
+        if ($query) {
+            $array["success"] = "1";
+        }
+        echo json_encode($array);
     }
 }
