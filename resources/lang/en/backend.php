b9bb5196966552cb24b238b03441ec7ba7df0aa4
Ashish <ashish336b@gmail.com>
2019-07-16 21:55:19 +0545

    asdf

--- resources/lang/en/backend.php
+++ resources/lang/en/backend.php
@@ -13,5 +13,5 @@ return [
     'manage_settings' => 'Manage Settings',
     //for pages distributor
     "note_above_button"=>'Visit all the above links, subscribe where you have to subscribe, read where you have to read to be able to proceed',
-    "checkbox_note"=>"I have watch the entire video",
+    "checkbox_note"=>"I have watched the entire video",
 ];
