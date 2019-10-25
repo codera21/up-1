75f00cc467abfc320f5474267f94f4adcd359503
Ashish <ashish336b@gmail.com>
2019-10-25 16:39:55 +0545

    redirect flow

--- app/Http/Middleware/formStep.php
+++ app/Http/Middleware/formStep.php
@@ -33,16 +33,16 @@ class formStep
         } else if (!$request->cookie("privacyandpolicy")) {
             $cookie2 = Cookie::make("privacyandpolicy", "privacypolicy");
             return redirect("pages/videos?id=$value")->withCookie($cookie2);
-        } /*else if (!$request->cookie('questions')) {
+        } else if (!$request->cookie('questions')) {
             $cookie3 = Cookie::make("questions", "questions");
             return redirect("pages/dnasbook-webinar-questions?id=$value")->withCookie($cookie3);
-        } else if (!$request->cookie('dnasbook-distributor-training-certificate')) {
+        }/* else if (!$request->cookie('dnasbook-distributor-training-certificate')) {
             $cookie4 = Cookie::make("dnasbook-distributor-training-certificate", "dnasbook-distributor-training-certificate");
             return redirect("pages/dnasbook-distributor-training-certificate?id=$value")->withCookie($cookie4);
-        } else if (!$request->cookie('certificate')) {
+        }*/ else if (!$request->cookie('certificate')) {
             $cookie5 = Cookie::make("certificate", "certificate");
             return redirect("pages/certificate?id=$value")->withCookie($cookie5);
-        }*/
+        }
         return $next($request);
     }
 }
