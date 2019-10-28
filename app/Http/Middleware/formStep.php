ac57456dc9abc7c120a4bc1ac4d42058e6b5cb68
codera21 <atul.nepal@gmail.com>
2019-10-28 10:08:35 +0545

    cookie logic

--- app/Http/Middleware/formStep.php
+++ app/Http/Middleware/formStep.php
@@ -24,23 +24,24 @@ class formStep
         $key = key($arrayofurl);
         $value = $arrayofurl[$key];
         $value = intval($value);
-        if (!$request->hasCookie("videoPage")) {
-            $cookie = Cookie::make("videoPage", "videoPage");
-            return redirect("pages/distributor?id=$value")->withCookie($cookie);
+        if (!$request->hasCookie("distributorPage")) {
+//            $cookie = Cookie::make("distributorPage", "1");
+            return redirect("pages/distributor?id=$value");
+
         } elseif (!$request->cookie("payment-page")) {
-            $cookie1 = Cookie::make("payment-page", 'payment-page');
+            $cookie1 = Cookie::make("payment-page", '1');
             return redirect("pages/dnasbook-distributor-payment?id=$value")->withCookie($cookie1);
         } else if (!$request->cookie("privacyandpolicy")) {
-            $cookie2 = Cookie::make("privacyandpolicy", "privacypolicy");
+            $cookie2 = Cookie::make("privacyandpolicy", "1");
             return redirect("pages/videos?id=$value")->withCookie($cookie2);
         } else if (!$request->cookie('questions')) {
-            $cookie3 = Cookie::make("questions", "questions");
+            $cookie3 = Cookie::make("questions", "1");
             return redirect("pages/dnasbook-webinar-questions?id=$value")->withCookie($cookie3);
         }/* else if (!$request->cookie('dnasbook-distributor-training-certificate')) {
             $cookie4 = Cookie::make("dnasbook-distributor-training-certificate", "dnasbook-distributor-training-certificate");
             return redirect("pages/dnasbook-distributor-training-certificate?id=$value")->withCookie($cookie4);
         }*/ else if (!$request->cookie('certificate')) {
-            $cookie5 = Cookie::make("certificate", "certificate");
+            $cookie5 = Cookie::make("certificate", "1");
             return redirect("pages/certificate?id=$value")->withCookie($cookie5);
         }
         return $next($request);
