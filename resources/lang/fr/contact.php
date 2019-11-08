1bd14f80beef69f680937b66e76028e4482b2530
codera21 <atul.nepal@gmail.com>
2019-11-08 18:23:55 +0545

    fr address changed

--- resources/lang/fr/contact.php
+++ resources/lang/fr/contact.php
@@ -10,9 +10,9 @@ return [
     'telephone' => 'Téléphone',
     'dashboard_video' => "<div style='position:relative;height:0;padding-bottom:56.25%'><iframe class='sproutvideo-player' src='https://videos.sproutvideo.com/embed/1c9ddcb71313e4cd94/98292d5df1187b61?playerTheme=dark&amp;playerColor=2f3437' style='position:absolute;width:100%;height:100%;left:0;top:0' frameborder='0' allowfullscreen></iframe></div>",
     'address_details' => "<strong>Siège mondial:</strong><br/>
-    1516 Maple Ave, #207<br/>
+    1215 Liberty Avenue<br/>
     HILLSIDE, NJ 07205 (USA)<br/>
-    Le Standard: 862 214 6868 (USA). En dehors des États-Unis: +1 862 214 6868.<br/>
+    Répondeur automatique: 862 214 6868 (USA). En dehors des États-Unis: +1 862 214 6868.<br/>
     Direct: 862 214 6863 (USA). En dehors des États-Unis: +1 862 214 6863.<br/>
     Fax: 862 214 6868/973 926 0890 (USA). En dehors des États-Unis: +1 862 214 6863 / + 1 973 926 0890.<br/>
     Cellphone & WhatSapp: 908 249 7637 (USA). En dehors des États-Unis: +1 908 249 7637.<br/>
@@ -20,7 +20,7 @@ return [
     <strong>Siège Africain:</strong><br/>
     Biyem-assi/Mendong/Carrefour TKC<br/>
     Immeuble CLARA, Porte 6, Cameroon, Yaoundé<br/>
-    Phone & Fax: 222 314 866 (Cameroon). En dehors du Cameroon: (+237) 222 314 866<br/>
-    Cellphone: 680 64 82 11 (Cameroon). En dehors du Cameroon: (+237) 680 64 82 11<br/>
-    WhatSapp: +225 07 96 40 75.",
+    Phone & Fax: 222 314 866 (Cameroon). Outside Cameroon: (+237) 222 314 866<br/>
+    Cellphone & WhatSapp: 672 46 05 04 (Cameroun). En dehors de Cameroun: (+237) 672 46 05 04).<br/>
+    Cellphone & WhatSapp: 908 249 7637 (USA). En dehors des États-Unis: +1 908 249 7637.",
 ];
