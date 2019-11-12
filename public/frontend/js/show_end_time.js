d0829390b6164f0845fc10e53c6a8882f90089b3
Naveed <mughal.naveed@gmail.com>
2019-11-12 12:00:28 +0500

    Payment Token Expiry Issue Fixed

--- public/frontend/js/show_end_time.js
+++ public/frontend/js/show_end_time.js
@@ -11,14 +11,15 @@ $(document).ready(function(){
 function show_end_time(){
 	var started_at = new Date(	new Date().toLocaleString('en-US', {timeZone: timezone}) );
 	
-	var diff = end_at.getTime() - started_at.getTime(); // this is a time in milliseconds
-	var hours = parseInt(Math.floor(diff / 1000 / 60 / 60));
+	var diff = eval(end_at.getTime() - started_at.getTime()); // this is a time in milliseconds
+	
+	var hours = eval(Math.floor(diff / 1000 / 60 / 60));
 	
 	diff -= hours * 1000 * 60 * 60;
-	var minutes = parseInt(Math.floor(diff / 1000 / 60));
+	var minutes = eval(Math.floor(diff / 1000 / 60));
 	
 	diff -= minutes * 1000 * 60;
-	var seconds = parseInt(Math.floor(diff / 1000));
+	var seconds = eval(Math.floor(diff / 1000));
 	
 	// If using time pickers with 24 hours format, add the below line get exact hours
 	
